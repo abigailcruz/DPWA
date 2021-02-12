9070bace59d036d4dcfaf33f97a15492a4a07d23
abigailcruz <jhojairamarquez.99@gmail.com>
2021-02-12 08:54:40 -0600

    Implementacion de Repositorio

---
+++ Laboratorio1DATA/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using Laboratorio1Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Laboratorio1DATA.Interfaces
+{
+    interface IRepository<T> where T : EntityBase
+    {
+        T GetById(int Id);
+        IEnumerable<T> List();
+        IEnumerable<T> List(Expression<Func<T, bool>> predicate);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
---
+++ Laboratorio1DATA/Repository.cs
@@ -0,0 +1,43 @@
+﻿using Laboratorio1DATA.Interfaces;
+using Laboratorio1Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Laboratorio1DATA
+{
+    public class Repository<T> : IRepository<T> where T : EntityBase
+    {
+      
+        public T GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> List(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
